a3efd64e99efc8684d2e9d8f58ea5b38cb1378b1
JuanEspitia14 <juandavid.espitia@mercadolibre.com.co>
2023-03-18 17:19:07 -0500

    test5

--- app/IrisFlowerML.py
+++ app/IrisFlowerML.py
@@ -1,8 +1,7 @@
-# -*- coding: utf-8 -*-
 # DataFlair Iris Flower Classification
 # Import Packages
+import matplotlib
 import numpy as np
 import matplotlib.pyplot as plt
+import seaborn as sns
 import pandas as pd
-
-print("hello world")
